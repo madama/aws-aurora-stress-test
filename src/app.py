824dd1460bbd49e1f31662c36ce0019a13a0e660
Daniele Madama <d.madama@gmail.com>
2021-01-24 16:31:33 +0100

    Change connect parameters

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ from codeguru_profiler_agent import with_lambda_profiler
 
 @with_lambda_profiler()
 def lambda_handler(event, context):
-    print(context)
-    print(event)
+    #print(context)
+    #print(event)
     message = "hello world"
     if (event["queryStringParameters"] and "doIt" in event["queryStringParameters"] and event["queryStringParameters"]["doIt"] == "true"):
         if ("DB_HOST" in os.environ and "DB_USER" in os.environ and "DB_PASSWORD" in os.environ and "DB_NAME" in os.environ):
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
                 password = os.environ["DB_PASSWORD"]
                 dbname = os.environ["DB_NAME"]
                 print(host, user, password, dbname)
-                conn = pymysql.connect(host, user=user, port=3306, passwd=password, db=dbname)
+                conn = pymysql.connect(host=host, user=user, port=3306, password=password, database=dbname)
                 cursor = conn.cursor()                                    
                 cursor.execute("call eat_cpu(10000);")
                 for result in cursor.fetchall():
